36a8073f31c7a3a8833022aaf097f8f15f1bf9da
Evan Nguyen <46007234+nguyened@users.noreply.github.com>
2019-09-19 00:58:53 -0400

    Update MatrixOps.java

--- ee363_Soft_Gen/hw1/MatrixOps.java
+++ ee363_Soft_Gen/hw1/MatrixOps.java
@@ -11,10 +11,31 @@ public class MatrixOps
     }
     return result;
   };
-  // static double[][] mult(double[][] a, double[][] b)
-  // {
-  //   return;
-  // };
+  static double[][] mult(double[][] a, double[][] b)
+  {
+    // matirices must be compatible, A columns = B rows
+    // answer column size = B column size
+    // answer rows size = A row size
+    double[][] result = new double[3][3]; // initialized to zero
+    for(int i = 0; i<3; i++)
+    {
+      for(int j = 0; j<3; j++)
+      {
+        for(int k = 0; k<3; k++)
+        {
+          if(j>0)
+          {
+            result[i][k] += a[i][j]*b[j][k];
+          } else
+            {
+              result[i][k] = a[i][j]*b[j][k];
+            }
+        }
+      }
+    }
+
+    return result;
+  };
   // static double[][] transpose(double[][] a)
   // {
   //   return;
