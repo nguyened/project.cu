7cb7c80f2fc0752cd5c4f43f287b5e6f13a075c7
Evan Nguyen <46007234+nguyened@users.noreply.github.com>
2019-09-20 18:04:54 -0400

    Update ClientMatrix.java

--- ee363_Soft_Gen/hw1/ClientMatrix.java
+++ ee363_Soft_Gen/hw1/ClientMatrix.java
@@ -7,6 +7,13 @@
 */
 public class ClientMatrix
 {
+  public static void PrintArry(double[] smpl)
+  {
+    for(int i=0;i<3;i++)
+    {
+      System.out.println("Result: row: "+i+" val: "+smpl[i]);
+    }
+  }
   public static void Print2DArry(double[][] smpl)
   {
     for(int i=0;i<3;i++)
@@ -24,19 +31,14 @@ public class ClientMatrix
     double[] vector2 = new double[]{4.0,8.0,10.0};
     double[][] mat1 = new double[][]{ {1.0,0.0,1.0},{0.0,1.0,2.0},{0.0,0.0,0.0} };
     double[][] mat2 = new double[][]{ {3.0,5.0,0.0},{-1.0,0.0,1.0},{2.0,-1.0,0.0} };
-    double[][] wtf = new double[3][3];
     System.out.println("Dot: " + MatrixOps.dot(vector1,vector2));
-    // for(int i=0;i<3;i++)
-    // {
-    //   for(int j=0;j<3;j++)
-    //   {
-    //     System.out.println("Mat2: row: "+i+" col: "+j+" val: "+mat2[i][j]);
-    //   }
-    // }
-    // Print2D(wtf);
     Print2DArry(MatrixOps.mult(mat1,mat2));
     System.out.println("[transpose]");
     Print2DArry(MatrixOps.transpose(mat2));
+    System.out.println("[m-v]");
+    PrintArry(MatrixOps.mult(mat2,vector1));
+    System.out.println("[v-m]");
+    PrintArry(MatrixOps.mult(vector1,mat2));
     System.out.println("[done]");
   }
 }
