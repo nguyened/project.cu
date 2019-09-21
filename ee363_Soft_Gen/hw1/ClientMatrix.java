50643e1e408901f987e0222439746d65ce0bfef6
Evan Nguyen <46007234+nguyened@users.noreply.github.com>
2019-09-20 20:53:48 -0400

    Update ClientMatrix.java

--- ee363_Soft_Gen/hw1/ClientMatrix.java
+++ ee363_Soft_Gen/hw1/ClientMatrix.java
@@ -1,24 +1,26 @@
 /*
- * @purpose Demostrates usage of MatrixOps
- *
- * @author Evan Nguyen
- *
+ * @purpose EE363 - HW1 - Demostrates usage of MatrixOps
+ * Evan Nguyen CE
+ * Paul Schmid SE
  * @modified Sep 20, 2019
 */
 public class ClientMatrix
 {
   public static void PrintArry(double[] smpl)
   {
-    for(int i=0;i<3;i++)
+    int arrySizeRow = smpl.length;
+    for(int i=0;i<arrySizeRow;i++)
     {
       System.out.println("row: "+i+"   val: "+smpl[i]);
     }
   }
   public static void Print2DArry(double[][] smpl)
   {
-    for(int i=0;i<3;i++)
+    int arrySizeRow = smpl.length;
+    int arrySizeCol = smpl[0].length;
+    for(int i=0;i<arrySizeRow;i++)
     {
-      for(int j=0;j<3;j++)
+      for(int j=0;j<arrySizeCol;j++)
       {
         System.out.println("row: "+i+" col: "+j+"   val: "+smpl[i][j]);
       }
@@ -28,26 +30,26 @@ public class ClientMatrix
   {
     double[] vector1 = new double[]{9.0,2.0,7.0};
     double[] vector2 = new double[]{4.0,8.0,10.0};
-    double[][] mat1 = new double[][]{ {1.0,0.0,1.0},{0.0,1.0,2.0},{0.0,0.0,0.0} };
-    double[][] mat2 = new double[][]{ {3.0,5.0,0.0},{-1.0,0.0,1.0},{2.0,-1.0,0.0} };
+    double[][] mat3x3 = new double[][]{ {3.0,5.0,0.0},{-1.0,0.0,1.0},{2.0,-1.0,0.0} };
+    double[][] mat3x2 = new double[][]{ {1.0,2.5},{3.1,1.0},{1.0,2.0} };
     System.out.println("[start]");
-    System.out.println("\n[vector1]");
+    System.out.println("\n[vector 1]");
     PrintArry(vector1);
-    System.out.println("\n[vector2]");
+    System.out.println("\n[vector 2]");
     PrintArry(vector2);
-    System.out.println("\n[matrix1]");
-    Print2DArry(mat1);
-    System.out.println("\n[matrix2]");
-    Print2DArry(mat2);
+    System.out.println("\n[matrix (3x3)]");
+    Print2DArry(mat3x3);
+    System.out.println("\n[matrix (3x2)]");
+    Print2DArry(mat3x2);
     System.out.println("\n[Vector Dot Product]: " + MatrixOps.dot(vector1,vector2));
-    System.out.println("\n[matrix-matrix]: using mult()");
-    Print2DArry(MatrixOps.mult(mat1,mat2));
-    System.out.println("\n[transpose matrix]: using transpose()");
-    Print2DArry(MatrixOps.transpose(mat2));
-    System.out.println("\n[matrix-vector]: using mult()");
-    PrintArry(MatrixOps.mult(mat2,vector1));
-    System.out.println("\n[vector-matrix]: using mult()");
-    PrintArry(MatrixOps.mult(vector1,mat2));
+    System.out.println("\n[matrix - matrix]: using mult()");
+    Print2DArry(MatrixOps.mult(mat3x3,mat3x2));
+    System.out.println("\n[transpose matrix 3x2]: using transpose()");
+    Print2DArry(MatrixOps.transpose(mat3x2));
+    System.out.println("\n[matrix 3x3 - vector 1]: using mult()");
+    PrintArry(MatrixOps.mult(mat3x3,vector1));
+    System.out.println("\n[vector 1 - matrix 3x3]: using mult()");
+    PrintArry(MatrixOps.mult(vector1,mat3x3));
     System.out.println("\n[done]");
   }
 }
