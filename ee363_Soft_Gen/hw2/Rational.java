3c6176297709d6b9ebbcd746ea4c3bd3ca139b78
Evan Nguyen <46007234+nguyened@users.noreply.github.com>
2019-09-26 16:23:10 -0400

    Create Rational.java

---
+++ ee363_Soft_Gen/hw2/Rational.java
@@ -0,0 +1,16 @@
+/*
+ * @purpose EE363 - HW2 - Rational
+ * @authors Evan Nguyen CE , Paul Schmid SE
+ * @modified Sep 25, 2019
+*/
+public class Rational
+{
+  public static void main(String[] args)
+  {
+    System.out.println("[start]");
+    int n1=5, d1=6, n2=3, d2=4;
+    System.out.println("r1: "+n1+"/"+d1);
+    System.out.println("r2: "+n2+"/"+d2);
+    System.out.println("\n[done]");
+  }
+}
