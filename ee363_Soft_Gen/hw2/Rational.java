c50c2247d0983e6a11b5bccba71047fe92833be2
Evan Nguyen <46007234+nguyened@users.noreply.github.com>
2019-09-26 16:52:34 -0400

    Update Rational.java

--- ee363_Soft_Gen/hw2/Rational.java
+++ ee363_Soft_Gen/hw2/Rational.java
@@ -5,12 +5,21 @@
 */
 public class Rational
 {
+  public static int[] Radd(int a,int b,int c,int d)
+  {
+    int[] result = new int[2];
+    result[0] = a+c;
+    result[1] = 0; //testing
+    return result;
+  }
   public static void main(String[] args)
   {
-    System.out.println("[start]");
+    System.out.println("[start]\n");
     int n1=5, d1=6, n2=3, d2=4;
-    System.out.println("r1: "+n1+"/"+d1);
-    System.out.println("r2: "+n2+"/"+d2);
+    int[] output = new int[2];
+    output = Radd(n1,d1,n2,d2);
+    System.out.println("r1: "+n1+"/"+d1+"\nr2: "+n2+"/"+d2);
+    System.out.println("r1 + r2: "+output[0]+"/"+output[1]);
     System.out.println("\n[done]");
   }
 }
